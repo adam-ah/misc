a10277385f265a6319770b235f8ebb2efa3fafd7
Adam Horvath <adam@nethosting.hu>
2013-08-20 16:24:10 +1000

    Auto resizing

--- code39/writeCode39.js
+++ code39/writeCode39.js
@@ -1,23 +1,27 @@
 var code39 = code39 || new Object();
 
 code39.createLetter = function(letter){
-    var div = document.createElement('div');
     if(letter == '*'){
         letter = 'star';
     }
-    div.setAttribute('class', 'code39 c' + letter);
-    return div;
+    var img = document.createElement('img');
+    img.setAttribute('src', 'images/' + letter + '.png');
+    img.setAttribute('class', 'code39 c' + letter);
+    var td = document.createElement('td');
+    td.appendChild(img);
+    return td;
 }
 
 code39.createCode = function(numbers){
     var charArr = ('*' + numbers + '*').split('');
-    var parent = document.createElement('div');
-    charArr.forEach(function(c){
+    var table = document.createElement('table');
+    table.setAttribute('class', 'code39');
+    var tr = document.createElement('tr');
+    table.appendChild(tr);
+    for(var i=0, c; c=charArr[i]; i++)
+    {
         var letter = code39.createLetter(c);
-        parent.appendChild(letter);
-    });
-    var close = document.createElement('div');
-    close.setAttribute('class', 'clear');
-    parent.appendChild(close);
-    return parent;
+        tr.appendChild(letter);
+    }
+    return table;
 }
