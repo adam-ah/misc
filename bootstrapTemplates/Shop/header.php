1c234fd751f0447e31695ee605001347c916a7da
Adam Horvath <adam@nethosting.hu>
2013-08-27 10:41:23 +1000

    Bootstrap templates

---
+++ bootstrapTemplates/Shop/header.php
@@ -0,0 +1,107 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Bootshop online Shopping cart</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+<!--Less styles -->
+   <!-- Other Less css file //different less files has different color scheam
+	<link rel="stylesheet/less" type="text/css" href="themes/less/simplex.less">
+	<link rel="stylesheet/less" type="text/css" href="themes/less/classified.less">
+	<link rel="stylesheet/less" type="text/css" href="themes/less/amelia.less">  MOVE DOWN TO activate
+	-->
+	<!--<link rel="stylesheet/less" type="text/css" href="themes/less/bootshop.less">
+	<script src="themes/js/less.js" type="text/javascript"></script> -->
+	
+<!-- Bootstrap style --> 
+    <link id="callCss" rel="stylesheet" href="themes/bootshop/bootstrap.min.css" media="screen"/>
+    <link href="themes/css/base.css" rel="stylesheet" media="screen"/>
+<!-- Bootstrap style responsive -->	
+	<link href="themes/css/bootstrap-responsive.min.css" rel="stylesheet"/>
+	<link href="themes/css/font-awesome.css" rel="stylesheet" type="text/css">
+<!-- Google-code-prettify -->	
+	<link href="themes/js/google-code-prettify/prettify.css" rel="stylesheet"/>
+<!-- fav and touch icons -->
+    <link rel="shortcut icon" href="themes/images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="themes/images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="themes/images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="themes/images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="themes/images/ico/apple-touch-icon-57-precomposed.png">
+	<style type="text/css" id="enject"></style>
+  </head>
+<body>
+<div id="header">
+<div class="container">
+<div id="welcomeLine" class="row">
+	<div class="span6">Welcome!<strong> User</strong></div>
+	<div class="span6">
+	<div class="pull-right">
+		<a href="product_summary.html"><span class="">Fr</span></a>
+		<a href="product_summary.html"><span class="">Es</span></a>
+		<span class="btn btn-mini">En</span>
+		<a href="product_summary.html"><span>&pound;</span></a>
+		<span class="btn btn-mini">$155.00</span>
+		<a href="product_summary.html"><span class="">$</span></a>
+		<a href="product_summary.html"><span class="btn btn-mini btn-primary"><i class="icon-shopping-cart icon-white"></i> [ 3 ] Itemes in your cart </span> </a> 
+	</div>
+	</div>
+</div>
+<!-- Navbar ================================================== -->
+<div id="logoArea" class="navbar">
+<a id="smallScreen" data-target="#topMenu" data-toggle="collapse" class="btn btn-navbar">
+	<span class="icon-bar"></span>
+	<span class="icon-bar"></span>
+	<span class="icon-bar"></span>
+</a>
+  <div class="navbar-inner">
+    <a class="brand" href="index.html"><img src="themes/images/logo.png" alt="Bootsshop"/></a>
+		<form class="form-inline navbar-search" method="post" action="products.html" >
+		<input id="srchFld" class="srchTxt" type="text" />
+		  <select class="srchTxt">
+			<option>All</option>
+			<option>CLOTHES </option>
+			<option>FOOD AND BEVERAGES </option>
+			<option>HEALTH & BEAUTY </option>
+			<option>SPORTS & LEISURE </option>
+			<option>BOOKS & ENTERTAINMENTS </option>
+		</select> 
+		  <button type="submit" id="submitButton" class="btn btn-primary">Go</button>
+    </form>
+    <ul id="topMenu" class="nav pull-right">
+	 <li class=""><a href="special_offer.html">Specials Offer</a></li>
+	 <li class=""><a href="normal.html">Delivery</a></li>
+	 <li class=""><a href="contact.html">Contact</a></li>
+	 <li class="">
+	 <a href="#login" role="button" data-toggle="modal" style="padding-right:0"><span class="btn btn-large btn-success">Login</span></a>
+	<div id="login" class="modal hide fade in" tabindex="-1" role="dialog" aria-labelledby="login" aria-hidden="false" >
+		  <div class="modal-header">
+			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+			<h3>Login Block</h3>
+		  </div>
+		  <div class="modal-body">
+			<form class="form-horizontal loginFrm">
+			  <div class="control-group">								
+				<input type="text" id="inputEmail" placeholder="Email">
+			  </div>
+			  <div class="control-group">
+				<input type="password" id="inputPassword" placeholder="Password">
+			  </div>
+			  <div class="control-group">
+				<label class="checkbox">
+				<input type="checkbox"> Remember me
+				</label>
+			  </div>
+			</form>		
+			<button type="submit" class="btn btn-success">Sign in</button>
+			<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+		  </div>
+	</div>
+	</li>
+    </ul>
+  </div>
+</div>
+</div>
+</div>
+<!-- Header End====================================================================== -->
---
+++ bootstrapTemplates/Shop/readme.txt
@@ -0,0 +1,79 @@
+/*
+Bootshop 1.1
+Date:3rd December 2012.
+Need help? : Contact
+=============================
+-----------------------------
+	Adhikari Surya
+	Frontend Web Developer
+	Email: adh.surya@gmail.com
+-----------------------------
+=============================
+Introduction
+============
+Bootshop templates is highly professional, clean and Google friendly html5 and css3 code template. 
+This professional online shopping cart is supported in all devices (such as desktop, iPad and iPhone) and compatible in most of the popular browser.
+This template is suitable for any kinds of online shopping cart.
+Different resources and technologies has been implemented at the time of development. So, this is very flexible and easy to extend for new features and pages.
+
+The clean and modern look allows you to use the theme for every kind of online shop.
+In combination with the powerful option to change the colour of all elements, you will be given the possibility to make a unique e-commerce template that stands out .  
+As your test of colour you may use different bootstrap based **BOOTSWATCH  css**  or **BOOTSWATCH  Less**  (http://bootswatch.com/) . 
+
+Anyone can easily add and remove components as it's based on the Bootstrap framework. Normally all the frontend pages are included in the file.
+This shopping cart code is  included less.js ( www.lesscss.org ) for compilation of  less file,
+Moreover, Font Awesome is in use as a icon (http://fortawesome.github.com). 
+So it does not matter whatever be the themes color icon will change automatically like font color.
+ 
+All the themes from Bootswatch are supported by this template which is already included in the package.
+
+Features:
+=========
+
+1. Available with 12 different themes in 20 Differnet patterns.
+3. Clean and professional design + PSD banner and logo.
+4. Less file included
+5. Validate with **w3c**.
+6. **BOOTSHOP** is 100% responsive template  which is suitable in different devices and systems such as desktops,  tablets and mobiles screen.
+7. Font Awesome is in use as a icon
+
+
+Pages Included
+===============
+
+1.	Home
+2.	Product page
+3.	Special offer page
+4.	Product details page
+5.	Products compare page
+6.	Product summary page
+7.	Login / Forgot password
+8.	Registration page
+9.	Contact us
+10.	Normal informations page
+11.	FAQ
+12.	***COMPONENTS of PAGES***
+
+
+Additional Credits:
+==================
+1. Twitter Bootstrap (Version 2.2.1)
+2. http://bootswatch.com 
+3.  www.lesscss.org
+4. http://www.sxc.hu
+5. http://www.google.com/webfonts
+6. http://fortawesome.github.com/Font-Awesome
+7. http://subtlepatterns.com
+8. http://leandrovieira.com for light box
+
+**************************************************************
+===================
+* PLEASE Email if more informations needed.
+* if You provide me email address I will let you know about new updates.
+=============================
+-----------------------------
+	Adhikari Surya
+	Frontend Web Developer
+	Email: adh.surya@gmail.com
+-----------------------------
+=============================
